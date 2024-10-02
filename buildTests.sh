471e6e3414e48d3c5ca22298a895a51b2c75d001
Michael Pingleton <mnpingleton@gmail.com>
2024-10-02 13:11:09 -0400

    Added makeshift unit test.

---
+++ buildTests.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+clang++ -o ./binary/testmain ./tests/testmain.cpp ./source/address.cpp ./source/geometry.cpp ./source/image.cpp ./source/partition.cpp ./source/sector.cpp
---
+++ tests/testmain.cpp
@@ -0,0 +1,41 @@
+//-----------------------------------------------------------------------------
+// MBR Image Tools
+// 
+// testmain.cpp
+//
+// Temporary main function for makeshift unit testing.
+//
+// Created	: 2 October 2024
+// Author	: Michael Pingleton
+//-----------------------------------------------------------------------------
+
+#include "../include/mbrimage.hpp"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout << "Hello world!" << endl;
+
+	SectorAddress s = SectorAddress();
+	DriveGeometry g = DriveGeometry();
+	MbrPartition p = MbrPartition();
+
+	cout << s.toInfoString() << endl << g.toInfoString() << endl << p.toInfoString() << endl << endl;
+
+	MbrDiskImage diskImage = MbrDiskImage();
+
+	// Standard 3.5-inch diskette.
+	diskImage.geometry = DriveGeometry();
+	diskImage.geometry.bytes = 512;
+	diskImage.geometry.sectors = 18;
+	diskImage.geometry.heads = 2;
+	diskImage.geometry.cylinders = 80;
+
+	cout << diskImage.toInfoString() << endl;
+
+	return 0;
+}
